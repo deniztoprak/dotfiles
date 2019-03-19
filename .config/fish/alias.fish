1abedcb2eafabbfa0b37824b4aa9ef1300bb6738
Deniz Toprak <deniz.toprak@maibornwolff.de>
2019-03-19 17:47:26 +0100

    Add fish config files

---
+++ .config/fish/alias.fish
@@ -0,0 +1,35 @@
+alias g='git'
+#compdef g=git
+
+alias gst='git status -s'
+#compdef _git gst=git-status
+
+alias gd='git diff'
+#compdef _git gd=git-diff
+
+alias gdc='git diff --cached'
+#compdef _git gdc=git-diff
+
+alias gl='git pull'
+#compdef _git gl=git-pull
+
+alias gp='git push'
+#compdef _git gp=git-push
+
+alias gc='git commit -v'
+#compdef _git gc=git-commit
+
+alias gco='git checkout'
+#compdef _git gco=git-checkout
+
+alias ga='git add'
+#compdef _git ga=git-add
+#
+alias gaa='git add --all'
+#compdef _git gaa=git-add
+
+alias gm='git merge'
+#compdef _git gm=git-merge
+
+alias glg='git log --stat --max-count=10'
+#compdef _git glg=git-log
---
+++ .config/fish/config.fish
@@ -0,0 +1,5 @@
+. ~/.config/fish/alias.fish
+
+set -x LANG en_US.UTF-8
+set fish_color_search_match --background=grey
+set fish_greeting
